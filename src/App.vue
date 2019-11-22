6adf84e2e643278c5ff065cbdeb5e679322a1efe
xutongkai <1781746787@qq.com>
2019-11-22 10:14:36 +0800

    blackhorse

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <template>
-  <div id="app">
-    <div id="nav">
+  <div class="app">
+    <mt-header fixed title="blackhorse"></mt-header>
+    <div class="nav">
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link>
     </div>
@@ -9,24 +10,8 @@
 </template>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
+.app {
+  padding-top:40px;
+  box-sizing: border-box;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -3,6 +3,11 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
+import 'mint-ui/lib/style.css'
+import { Header } from 'mint-ui';
+Vue.component(Header.name, Header);
+
+
 Vue.config.productionTip = false
 
 new Vue({
